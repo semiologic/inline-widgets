1ac77205f68815a9a6ed1dc9cc4ef271d2c87a9b
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-23 11:37:35 +0000

    bump versions
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/inline-widgets/trunk@635 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inline-widgets.php
+++ inline-widgets.php
@@ -3,7 +3,7 @@
 Plugin Name: Inline Widgets
 Plugin URI: http://www.semiologic.com/software/widgets/inline-widgets/
 Description: Creates a special sidebar that lets you insert widget in posts and pages. Configure these widgets under Design / Widgets, by selecting the Inline Widgets sidebar.
-Version: 1.0.1 alpha
+Version: 1.0.1 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Update Package: https://members.semiologic.com/media/plugins/inline-widgets/inline-widgets.zip
